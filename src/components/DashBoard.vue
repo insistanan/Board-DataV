0a8c5170ba6313ed9e56b75c42271a9c8c25b7d6
anan <1530885478@qq.com>
2023-11-13 21:59:26 +0800

    do some demo

---
+++ src/components/DashBoard.vue
@@ -0,0 +1,25 @@
+<template>
+  <dv-full-screen-container>
+    <div class="page-content">
+      <div class="box-header">工业互联网标识解析二级节点（鹰潭）</div>
+      <div class="box-prefix"></div>
+      <div class="box-center"></div>
+      <div class="box-suffix"></div>
+    </div>
+  </dv-full-screen-container>
+</template>
+
+<script>
+export default {
+  name: 'DashBoard',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
